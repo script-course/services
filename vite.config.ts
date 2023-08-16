ab75523da522649b00856c96b26de4f2e8449cfe
JasperBoerhof <jasperboerhof@gmail.com>
2023-08-16 10:13:02 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
     ],
+    build: {target: 'esnext'},
 });
